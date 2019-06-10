b3515b2062d73b8bf110195b385a37500084e28c
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-10 15:13:39 +0300

    Player class created.

--- Project/Tic-Tac-Toe Player/Player.cs
+++ Project/Tic-Tac-Toe Player/Player.cs
@@ -8,5 +8,33 @@ namespace Tic_Tac_Toe_Player
 {
     public class Player
     {
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+
+        private int age;
+        public int Age {
+            get => age;
+            private set {
+                if(value >= AgeConstraints.MinAge 
+                   && value < AgeConstraints.MaxAge)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        String.Format("Age of player should between {0} - {1}",
+                            AgeConstraints.MinAge, AgeConstraints.MaxAge)
+                    );
+                }
+            }
+        }
+
+        public Player(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
     }
 }
