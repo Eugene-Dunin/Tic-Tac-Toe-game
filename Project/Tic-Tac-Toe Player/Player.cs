6b917f49820384f7a5434d2dd1e49f47700c71e5
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-11 10:51:28 +0300

    Transfer age constraints of Player class inside.

--- Project/Tic-Tac-Toe Player/Player.cs
+++ Project/Tic-Tac-Toe Player/Player.cs
@@ -8,5 +8,39 @@ namespace Tic_Tac_Toe_Player
 {
     public class Player
     {
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+
+        private int age;
+        public int Age {
+            get => age;
+            private set {
+                if(value >= AgeConstraints.MinAge 
+                   && value < AgeConstraints.MaxAge)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        String.Format("Age of player should between {0} - {1}",
+                            AgeConstraints.MinAge, AgeConstraints.MaxAge)
+                    );
+                }
+            }
+        }
+
+        public Player(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public static class AgeConstraints
+        {
+            public const int MinAge = 3;
+            public const int MaxAge = 120;
+        }
     }
 }
