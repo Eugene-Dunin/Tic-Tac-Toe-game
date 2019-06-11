575181bef5b8f8fbe6a5f21b2cc9d8cc930ba77a
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-11 11:34:37 +0300

    User interaction insfrastructure create.

---
+++ Project/Tic-Tac-Toe Game/UserInteraction/Factoties/UserInteractorFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game.UserInteraction.Factoties
+{
+    class UserInteractorFactory: AbstractUserInteractorFactory
+    {
+        public override IUserInteractor GetInstance()
+        {
+            return UserInteractor.GetInstance();
+        }
+    }
+}
