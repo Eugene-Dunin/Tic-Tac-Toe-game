1416ebc2e4cd4b0491ece11c4ec79ec2a7509c90
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-11 11:43:08 +0300

    Game event args created.

---
+++ Project/Tic-Tac-Toe Game/Events/GameFinishedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Player;
+
+namespace Tic_Tac_Toe_Game.Events
+{
+    enum GameResult
+    {
+        Draw,
+        Win,
+        Winners
+    }
+
+    class GameFinishedEventArgs: EventArgs
+    {
+        private readonly GameResult gameResult;
+        private readonly Player[] players;
+
+        public GameFinishedEventArgs(
+            GameResult gameResult, Player[] players)
+        {
+            this.gameResult = gameResult;
+            this.players = players;
+        }
+
+        public GameResult GameResult { get => gameResult; }
+        public Player[] Players { get => players; }
+    }
+}
