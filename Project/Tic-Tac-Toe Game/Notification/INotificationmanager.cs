5ad331d9e6da518353db823f196a67dcc5508ced
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-11 13:23:32 +0300

    Created Notification infrastructure.

---
+++ Project/Tic-Tac-Toe Game/Notification/INotificationmanager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Game.Events;
+using Tic_Tac_Toe_Player;
+
+namespace Tic_Tac_Toe_Game.Notification
+{
+    interface INotificationManager
+    {
+        void ShowActivePlayer(Player player);
+        void ShowGameResults(GameFinishedEventArgs gameFinishedEventArgs);
+    }
+}
