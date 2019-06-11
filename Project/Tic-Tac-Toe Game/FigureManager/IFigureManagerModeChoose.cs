7f9b3947596209a03b7053af9e9bf0308db99531
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-11 12:39:40 +0300

    FigureManager infrastructure created.

---
+++ Project/Tic-Tac-Toe Game/FigureManager/IFigureManagerModeChoose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game.FigureManager
+{
+    enum FigureManagerMode
+    {
+        Random, Choose
+    }
+
+    interface IFigureManagerModeChoose
+    {
+        FigureManagerMode GetMode();
+    }
+}
