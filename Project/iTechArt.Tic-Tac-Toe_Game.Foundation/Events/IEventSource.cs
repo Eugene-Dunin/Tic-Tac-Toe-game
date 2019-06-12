f514f5058e599f7074f1e54fe53ca0d5f79f6cc0
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-12 18:52:09 +0300

    Event args classes and general event model added.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/Events/IEventSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game.Foundation.Events
+{
+    public interface IEventSource<T> where T : EventArgs
+    {
+        event EventHandler<T> EventReccipientsKeeper;
+        void OnEventHappend(T e);
+    }
+}
