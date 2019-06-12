a8222b0d93295db535c4ba12ff93601a58d2f54d
Eugene-Dunin <Yauheni.Dunin@itechart-group.com>
2019-06-12 19:04:26 +0300

    New User interaction infrastructure.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/UserInteraction/IUserInteractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Game.Events;
+using Tic_Tac_Toe_Game.Foundation.Events;
+using Tic_Tac_Toe_Game.Foundation.GameLogic;
+using Tic_Tac_Toe_Player;
+
+namespace Tic_Tac_Toe_Game
+{
+    public interface IUserInteractor
+    {
+        Player CreatePlayer();
+        int SetPlayersCount();
+        int SetGameGridSize();
+        bool RepeatGame();
+        CellCoordinates GetCellCoordinates();
+    }
+}
