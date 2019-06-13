5bf652d9c93f82ba7db4d9fcc85aa9a22cf60218
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-13 11:23:17 +0300

    Create player module.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/Player/AgeConstrains.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Player
+{
+    static class AgeConstraints
+    {
+        internal const int MinAge = 3;
+        internal const int MaxAge = 120;
+    }
+}
