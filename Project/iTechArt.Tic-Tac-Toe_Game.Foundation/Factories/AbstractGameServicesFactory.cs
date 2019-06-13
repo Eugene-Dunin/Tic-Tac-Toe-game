cd6b251b8e2cb81012ab5284334c99c08f13bf27
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-13 11:29:50 +0300

    Added abstract Factory for game services.
    It is necessary to provide the creation of game services.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/Factories/AbstractGameServicesFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Game.FigureManager;
+using Tic_Tac_Toe_Game.Foundation.Display;
+using Tic_Tac_Toe_Game.Foundation.GameLogic;
+using Tic_Tac_Toe_Game.Foundation.PlayersDataManager;
+using Tic_Tac_Toe_Game.Notification;
+
+namespace Tic_Tac_Toe_Game.Foundation.Factories
+{
+    public abstract class AbstractGameServicesFactory
+    {
+        public abstract IPlayersDataManager GetPlayersDataManager(int playersCount);
+        public abstract IUserInteractor GetUserInteractor();
+        public abstract INotificationManager GetNotificationManager();
+        public abstract IFigureManager GetFigureManager();
+        public abstract IGameBoardDisplay GameBoardDisplay();
+        public abstract IGameBoardStorage GetGameBoard();
+        public abstract IGameProgressManager GetGameProgressManager();
+    }
+}
