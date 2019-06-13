373fb9f8095d2cb8bfe617330d8b207534792510
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-13 11:27:21 +0300

    Add Players data manager.
    Provides functionality for managing data of players during the game.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/PlayersDataManager/IPlayersDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Game.Foundation.Figures;
+using Tic_Tac_Toe_Player;
+
+namespace Tic_Tac_Toe_Game.Foundation.PlayersDataManager
+{
+    public interface IPlayersDataManager
+    {
+        bool IsUniquePlayer(Player player);
+        bool Add(Player key, Figure value);
+        string GetPlayersList();
+        void SetFirstPlayer(int ind);
+        IEnumerable<KeyValuePair<Player, Figure>> GetNextPlayer(bool gameFinished);
+    }
+}
