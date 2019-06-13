28ae12602588eefb44e93f994e42358bfa7a4dcb
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-13 11:20:25 +0300

    Add figure manager interface .
    Need for players choose of figure that is it will play.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/FigureManager/IFigureManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Game.Foundation.Figures;
+
+namespace Tic_Tac_Toe_Game.FigureManager
+{
+    public interface IFigureManager
+    {
+        Figure GetFigure();
+    }
+}
