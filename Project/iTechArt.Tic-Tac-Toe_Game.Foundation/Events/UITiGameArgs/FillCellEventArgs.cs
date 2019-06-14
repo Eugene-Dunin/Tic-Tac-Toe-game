96c0a8da3d5eb9c00131e648b861f008bcf244e9
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-14 12:55:11 +0300

    Build in game dependence  event args and Interface for interaction with "Game" class.

---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/Events/UITiGameArgs/FillCellEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.TicTacToe.Foundation.Events.UIToGameArgs
+{
+    public class FillCellEventArgs : EventArgs
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public FillCellEventArgs(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
---
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/Events/UITiGameArgs/RegisterEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe_Player;
+
+namespace iTechArt.TicTacToe.Foundation.Events.UIToGameArgs
+{
+    public class RegisterEventArgs : EventArgs
+    {
+        public HashSet<Player> Players { get; }
+
+        public Player FirstPlayer { get; }
+
+
+        public RegisterEventArgs(HashSet<Player> Players)
+        {
+            this.Players = Players;
+        }
+    }
+}
