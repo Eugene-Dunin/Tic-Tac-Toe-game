b19080d0edf8cc439d049304894085886e3a2c95
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-14 17:05:38 +0300

    Reorganized stored files, new project settings.

--- Project/iTechArt.TicTacToe.Foundation/Players/Player.cs
+++ Project/iTechArt.TicTacToe.Foundation/Players/Player.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace iTechArt.TicTacToe.Foundation.Player
+namespace iTechArt.TicTacToe.Foundation.Players
 {
     public class Player
     {
