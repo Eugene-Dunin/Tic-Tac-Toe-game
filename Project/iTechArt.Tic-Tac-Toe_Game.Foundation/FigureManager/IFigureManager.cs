6d195c8d6a8b1c742dec966bcb9872f9efbeb262
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-14 12:59:08 +0300

    Fix figure manager.

--- Project/iTechArt.Tic-Tac-Toe_Game.Foundation/FigureManager/IFigureManager.cs
+++ Project/iTechArt.Tic-Tac-Toe_Game.Foundation/FigureManager/IFigureManager.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using iTechArt.Tic_Tac_Toe_Game.Foundation.Figures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tic_Tac_Toe_Game.Foundation.Figures;
 
-namespace Tic_Tac_Toe_Game.FigureManager
+namespace iTechArt.TicTacToe.Foundation.FigureManager
 {
     public interface IFigureManager
     {
-        Figure GetFigure();
+        IFigure GetFigure();
     }
 }
