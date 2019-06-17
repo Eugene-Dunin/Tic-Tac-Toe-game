ecba2e3d0ec97b8b4026c8ac0bfa5f6d90319cab
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-17 17:13:45 +0300

    Add new factories.

---
+++ Project/iTechArt.TicTacToe.Foundation/Interfaces/IProgressManagerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.TicTacToe.Foundation.Interfaces
+{
+    public interface IProgressManagerFactory
+    {
+        BaseProgressManager CreateProgressManager(IBoard boardFactory);
+    }
+}
