f4a8beef65c82f085561d7823e0e6884f42ea4cf
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-17 11:37:29 +0300

    Update security interaction model of figure points counter.

---
+++ Project/iTechArt.TicTacToe.Foundation/FigurePointsCounters/FigurePointsCounter.cs
@@ -0,0 +1,37 @@
+﻿using iTechArt.TicTacToe.Foundation.FigurePointsCounters;
+using iTechArt.TicTacToe.Foundation.Figures;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.TicTacToe.Foundation.Progress
+{
+    public class FigurePointsCounter : IFigurePointsCounter
+    {
+        public FigureType Type { get; }
+        public int NumberOfPoints { get; private set; }
+
+        public IncrementPointsResult IncrementPoints(Func<BaseProgressManager> getProgressManager)
+        {
+            if (getProgressManager != null 
+                && getProgressManager.Target is BaseProgressManager
+                && getProgressManager().ContainsFigurePointsCounters(this))
+            {
+                NumberOfPoints++;
+
+                return IncrementPointsResult.Successful;
+            }
+
+            return IncrementPointsResult.Forbidden;
+        }
+
+        public FigurePointsCounter(FigureType figureType)
+        {
+            Type = figureType;
+            NumberOfPoints = 0;
+        }
+    }
+}
