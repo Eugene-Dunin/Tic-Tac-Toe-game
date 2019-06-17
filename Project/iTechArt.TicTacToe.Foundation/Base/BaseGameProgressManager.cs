7834dc2ada90d278358000363911bdd14f64f4ad
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-17 10:34:37 +0300

    Abstract classes infrastructure added.

---
+++ Project/iTechArt.TicTacToe.Foundation/Base/BaseGameProgressManager.cs
@@ -0,0 +1,30 @@
+﻿using iTechArt.TicTacToe.Foundation.Events.GameToUIArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace iTechArt.TicTacToe.Foundation.Interfaces
+{
+    public abstract class BaseProgressManager
+    {
+        protected IBoard board;
+
+        protected HashSet<ILine> lines;
+
+        protected HashSet<IFigurePointsCounter> figurePointsCounters;
+
+        public EventHandler<GameFinishedEventArgs> GameFinished { get; set; }
+
+
+        protected BaseProgressManager(IBoard board)
+        {
+            this.board = board ?? throw new NullReferenceException();
+        }
+
+
+        public abstract void CalcGameProgress();
+    }
+}
---
+++ Project/iTechArt.TicTacToe.Foundation/Base/BasePlayersDataManager.cs
@@ -0,0 +1,26 @@
+﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+using iTechArt.TicTacToe.Foundation.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace iTechArt.TicTacToe.Foundation.Base
+{
+    public abstract class BasePlayersDataManager
+    {
+        private HashSet<Player> players;
+        private IFigureManager figureManager;
+
+        protected BasePlayersDataManager(IFigureManager figureManager)
+        {
+
+        }
+
+        public abstract bool Add(Player key);
+        public abstract void SetFirstPlayer(Player player);
+        public abstract IEnumerable<Player> GetNextPlayer(bool gameFinished);
+    }
+}
