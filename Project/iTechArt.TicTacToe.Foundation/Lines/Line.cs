cf757e18f9a8c7849ca184e33c09862768f2bdfd
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-17 10:29:49 +0300

    Line insfrastructure bisness implementation.

---
+++ Project/iTechArt.TicTacToe.Foundation/Lines/Line.cs
@@ -0,0 +1,39 @@
+﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.TicTacToe.Foundation.Lines
+{
+    class Line : ILine
+    {
+        private IReadOnlyList<ICell> cells;
+
+
+        public IFigure WinningFigure { get; private set; }
+
+
+        public LineState State { get; private set; }
+
+
+        public Line(IEnumerable<ICell> cells)
+        {
+            cells = cells ?? throw new NullReferenceException();
+            State = LineState.Progress;
+        }
+
+
+        public void CalcLineProgress()
+        {
+            if (State == LineState.Progress)
+            {
+                if (cells.All(cell => cell.Figure.Type == cells.First().Figure.Type))
+                {
+                    WinningFigure = cells.First().Figure;
+                }
+            }
+        }
+    }
+}
