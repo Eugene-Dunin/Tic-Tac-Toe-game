fa019662b01b2e99171ce4774831b1435ea53e59
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-19 16:48:40 +0300

    Create implementation of game config factory interface.

---
+++ Project/iTechArt.TicTacToe.Foundation/Interfaces/IGameConfigFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using iTechArt.TicTacToe.Foundation.Players;
+
+namespace iTechArt.TicTacToe.Foundation.Interfaces
+{
+    public interface IGameConfigFactory
+    {
+        IGameConfig CreateBaseGameConfigManager(ICollection<Player> players, Player firstPlayer, int boardSize);
+    }
+}
