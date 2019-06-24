0a1dfea4c6a75d882444f63e27ffa4d104ff314b
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-24 11:49:03 +0300

    Added figure factory implementation

---
+++ Project/iTechArt.TicTacToe.Foundation/Figures/FigureFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.Foundation.Figures
+{
+    public class FigureFactory : IFigureFactory
+    {
+        public IFigure CreateFigure(FigureType figureType)
+        {
+            switch (figureType)
+            {
+                case FigureType.Circle : return new Circle();
+                case FigureType.Cross : return new Cross();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(figureType), figureType, null);
+            }
+        }
+    }
+}
