9fbf88c761c265d64fee6b4aadc88c2d43018456
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-24 12:38:34 +0300

    Rewrite figure manager infrastructure.

---
+++ Project/TicTacToe/Interfaces/IFigureManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTechArt.TicTacToe.Foundation.Figures;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.Interfaces
+{
+    internal interface IFigureManager
+    {
+        IReadOnlyCollection<FigureType> AllFigureTypes { get; }
+
+        int CurrentAllowedCountOfPlayers { get; }
+
+        FigureType? PopFigureType(FigureType figureType);
+    }
+}
