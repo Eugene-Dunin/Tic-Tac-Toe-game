bb8c93902dde0631c5c0cf31a8e697c9e126eb35
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-26 18:54:54 +0300

    Delete outdated class + added IGameInputProvider interface implementation.

---
+++ Project/TicTacToe/InputManagers/GameInputProvider.cs
@@ -0,0 +1,41 @@
+﻿using iTechArt.TicTacToe.Console.Interfaces;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.InputManagers
+{
+    internal class InputProvider : IGameInputProvider
+    {
+        private readonly IConsoleInputProvider _inputProvider;
+
+
+        public InputProvider(IConsoleInputProvider inputProvider)
+        {
+            _inputProvider = inputProvider;
+        }
+
+
+        public  (int row, int col) GetCellCoordinates(IPlayer player)
+        {
+            (int row, int col) coordinates;
+            coordinates.row = _inputProvider.GetNumber("Input row number:");
+            coordinates.col = _inputProvider.GetNumber("Input column number:");
+            return coordinates;
+        }
+
+
+        protected override void SetPlayerInfo()
+        {
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("Incorrect age, it must be a number. Try again.");
+                    continue;
+                }
+                name = Console.ReadLine();
+                lastname = Console.ReadLine();
+                return;
+            } while (true);
+        }
+    }
+}
