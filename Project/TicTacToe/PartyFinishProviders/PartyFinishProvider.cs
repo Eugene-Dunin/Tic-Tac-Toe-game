aa5ae8a214f7aca3772acfe38e597cf9537fb82b
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-26 18:56:33 +0300

    Added implementation of party finish interface + added new project settings.

---
+++ Project/TicTacToe/PartyFinishProviders/PartyFinishProvider.cs
@@ -0,0 +1,27 @@
+﻿using iTechArt.TicTacToe.Console.Interfaces;
+using iTechArt.TicTacToe.Interfaces;
+
+namespace iTechArt.TicTacToe.PartyFinishProviders
+{
+    internal class PartyFinishProvider : IPartyFinishedProvider
+    {
+        private readonly IConsoleInputProvider _inputProvider;
+
+        public PartyFinishProvider(IConsoleInputProvider inputProvider)
+        {
+            _inputProvider = inputProvider;
+        }
+
+        public bool RepeatGame()
+        {
+            return _inputProvider.YesNoQuestion("Do you wish repeat game?",
+                "Incorrect answer. Please, print \"yes\" or \"no\".");
+        }
+
+        public bool CloseApp()
+        {
+            return _inputProvider.YesNoQuestion("Do you wish finish app?",
+                "Incorrect answer. Please, print \"yes\" or \"no\".");
+        }
+    }
+}
