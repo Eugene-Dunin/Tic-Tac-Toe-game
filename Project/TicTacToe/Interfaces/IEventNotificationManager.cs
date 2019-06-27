ae766b0fe801fd8ea4003f459f65417f1eb146e4
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-27 12:39:54 +0300

    Rename IUserNotificationManager to IEventNotificationManager. Fix interface and implementation.

---
+++ Project/TicTacToe/Interfaces/IEventNotificationManager.cs
@@ -0,0 +1,11 @@
+﻿using iTechArt.TicTacToe.Foundation.GameLogic.Finish;
+using iTechArt.TicTacToe.Foundation.GameLogic.StepDone;
+
+namespace iTechArt.TicTacToe.Interfaces
+{
+    internal interface IEventNotificationManager
+    {
+        void ShowWinner(FinishedEventArgs gameFinishedEventArgs);
+        void ShowStepDoneMessage(StepDoneEventArgs stepFailedEventArgs);
+    }
+}
---
+++ Project/TicTacToe/NotificationManagers/EventNotificationManager.cs
@@ -0,0 +1,49 @@
+﻿using iTechArt.TicTacToe.Console.Interfaces;
+using iTechArt.TicTacToe.Foundation.GameLogic.Finish;
+using iTechArt.TicTacToe.Foundation.GameLogic.StepDone;
+using iTechArt.TicTacToe.Interfaces;
+
+namespace iTechArt.TicTacToe.NotificationManagers
+{
+    internal class EventNotificationManager : IEventNotificationManager
+    {
+        private readonly IConsoleInputProvider _consoleInputProvider;
+
+        public EventNotificationManager(IConsoleInputProvider consoleInputProvider)
+        {
+            _consoleInputProvider = consoleInputProvider;
+        }
+
+        public void ShowWinner(FinishedEventArgs gameFinishedEventArgs)
+        {
+            switch (gameFinishedEventArgs.Result)
+            {
+                case GameResult.Draw:
+                    _consoleInputProvider.Console.WriteLine("Game result: Draw");
+                    break;
+                case GameResult.Win:
+                    var result = (WinFinishedEventArgs) gameFinishedEventArgs;
+                    _consoleInputProvider.Console.WriteLine("Game result: Win");
+                    _consoleInputProvider.Console.WriteLine("Win line have next cells:");
+                    /*result.WinLine.Cells.ToList().ForEach
+                        (cell => _consoleInputProvider.Console.WriteLine($"[{cell.Row}, {cell.Column}]"));*/
+                    _consoleInputProvider.Console.WriteLine();
+                    break;
+            }
+        }
+
+        public void ShowStepDoneMessage(StepDoneEventArgs stepDoneEventArgs)
+        {
+            switch (stepDoneEventArgs.Result)
+            {
+                case StepResult.CellNotExist:
+                    _consoleInputProvider.Console.WriteLine("Selected cell is not exist.");
+                    break;
+                case StepResult.CellIsFilled:
+                    var cell = ((CellIsFilledStepDoneEventArgs)stepDoneEventArgs).FilledCell;
+                    _consoleInputProvider.Console.WriteLine($"Cell on [{cell.Row}, {cell.Column}] filled by {nameof(cell.Figure.Type)}");
+                    break;
+            }
+        }
+    }
+}
