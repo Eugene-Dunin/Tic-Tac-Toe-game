5bad1bf8171ac6325281868c49dc93acad82385b
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-27 12:41:13 +0300

    Added implementation of IGameInputProvider interface.

---
+++ Project/TicTacToe/GameInputProviders/GameInputProvider.cs
@@ -0,0 +1,26 @@
+﻿using iTechArt.TicTacToe.Console.Interfaces;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.GameInputProviders
+{
+    internal class GameInputProvider : IGameInputProvider
+    {
+        private readonly IConsoleInputProvider _inputProvider;
+
+
+        public GameInputProvider(IConsoleInputProvider inputProvider)
+        {
+            _inputProvider = inputProvider;
+        }
+
+
+        public  (int row, int col) GetCellCoordinates(IPlayer player)
+        {
+            _inputProvider.Console.WriteLine($"Current player: {player.Name} {player.LastName}. Figure: {player.FigureType}");
+            (int row, int col) coordinates;
+            coordinates.row = _inputProvider.GetNumber("Input row number:");
+            coordinates.col = _inputProvider.GetNumber("Input column number:");
+            return coordinates;
+        }
+    }
+}
