4435fdd22604973b055ba350611d9c45cb6b273c
Eugene <Yauheni.Dunin@itechart-group.com>
2019-06-27 15:48:35 +0300

    According comments of pull request at 27.06.2019 fix done.

--- Project/iTechArt.TicTacToe.Foundation/Players/Player.cs
+++ Project/iTechArt.TicTacToe.Foundation/Players/Player.cs
@@ -5,9 +5,9 @@ namespace iTechArt.TicTacToe.Foundation.Players
 {
     public class Player : IPlayer
     {
-        public string Name { get; private set; }
+        public string Name { get; }
 
-        public string LastName { get; private set; }
+        public string LastName { get; }
 
         public FigureType FigureType { get; }
 
